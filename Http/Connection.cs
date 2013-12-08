bb6f4accdc6b458472fe192aceff31e3b77c5249
omeriko9 <omeriko9@gmail.com>
2013-12-08 22:40:35 +0200

    - missing file - remove static analysis

--- Http/Connection.cs
+++ Http/Connection.cs
@@ -7,6 +7,21 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Octokit.Internal;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyProductAttribute("Octokit")]
+[assembly: AssemblyVersionAttribute("0.1.5")]
+[assembly: AssemblyFileVersionAttribute("0.1.5")]
+[assembly: ComVisibleAttribute(false)]
+namespace System
+{
+    internal static class AssemblyVersionInformation
+    {
+        internal const string Version = "0.1.5";
+    }
+}
 
 namespace Octokit
 {
@@ -379,3 +394,6 @@ namespace Octokit
         }
     }
 }
+
+
+
